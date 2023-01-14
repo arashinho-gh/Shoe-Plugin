c683fd6b5dde67eda11d58cbe58cf87d897f392f
Damien Cheung <cheund11@mcmaster.ca>
2023-01-14 16:59:45 -0500

    simple form

--- deltahacks/src/App.js
+++ deltahacks/src/App.js
@@ -1,29 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./App.css";
+import { Button } from "antd";
+// import AuthContextProvider from "./contexts/AuthContextProvider";
+const data = {
+  athletic: {
+    Nike: -0.5,
+    Adidas: 0,
+    Puma: 0,
+    Reebok: -0.5,
+  },
+  dress: {
+    "Cole Haan": -0.5,
+    "Johnston & Murphy": 0,
+    Aldo: -0.5,
+    "Calvin Klein": -0.5,
+  },
+  casual: {
+    Converse: 0,
+    Vans: 0,
+    Toms: 0,
+    Sperry: 0,
+  },
+};
 
-function App() {
-
-  function test(){
-    /* eslint-disable no-undef */
-    chrome.tabs.query({active: true, currentWindow:true}, tabs=>{
-      const activeTabId = tabs[0].id;
-      chrome.scripting.executeScript(
-        {
-          target: {tabId: activeTabId},
-          //function: ()=>alert("React chrome extension alert")
-          function: ()=>{document.body.innerHTML = "Hello World"}
-        }
-      )
-    })
-  }
-
+const App = () => {
   return (
     <div className="App">
-      Enter Shoe Info:
-      <button onClick={test}>get shoe </button>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            UniversoleFits ShoeSizing Tool
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto"></ul>
+          </div>
+        </nav>
+        <form class="row g-3">
+          <div class="col-12">
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Choose Shoe Type</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div class="col-12">
+            <select class="form-select" aria-label="Default select example">
+            <option selected>Choose Shoe Brand</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div class="col-12">
+            <select class="form-select" aria-label="Default select example">
+            <option selected>Choose Shoe Size</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary">
+              Filter
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
