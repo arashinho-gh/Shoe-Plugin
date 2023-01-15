2c6aea1137c01345f6349b15b890f4c2739458eb
Damien Cheung <cheund11@mcmaster.ca>
2023-01-14 19:45:26 -0500

    form and styling

---
+++ deltahacks/src/shoe.js
@@ -0,0 +1,32 @@
+export const ShoeDataset = [
+  {
+    Gender: "Male",
+    Class: "running",
+    Brand: { Nike: -0.5, Adidas: 0, Reebok: 0.5 },
+  },
+  {
+    Gender: "Female",
+    Class: "running",
+    Brand: { Nike: 0, Adidas: 0.5, Reebok: 0.5 },
+  },
+  {
+    Gender: "Male",
+    Class: "dress",
+    Brand: {
+      "Cole Haan": -0.5,
+      "Johnston & Murphy": 0,
+      Aldo: -0.5,
+      "Calvin Klein": -0.5,
+    },
+  },
+  {
+    Gender: "Female",
+    Class: "dress",
+    Brand: {
+      "Cole Haan": -0.5,
+      "Johnston & Murphy": 0,
+      Aldo: -0.5,
+      "Calvin Klein": -0.5,
+    },
+  },
+];
